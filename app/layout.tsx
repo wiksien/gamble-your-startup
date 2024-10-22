409a998f60f94c37a262e48bec4c60c258ec153b
wiksien <88692323+wiksien@users.noreply.github.com>
2024-10-22 02:09:16 +0200

    Added umami tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,16 @@
 
 import { useState, useEffect } from 'react';
 import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Script from 'next/script'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Gamble Your Startup',
+  description: 'Generate random startup ideas',
+}
 
 export default function RootLayout({
   children,
@@ -25,7 +35,14 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body>
+      <head>
+        <Script
+          src="http://umami-kk0oo8wkosg8gggos84sc4k8.5.161.97.168.sslip.io/script.js"
+          data-website-id="9ea9b781-deb6-49a2-8e17-764fedb009da"
+          strategy="afterInteractive"
+        />
+      </head>
+      <body className={inter.className}>
         <button
           onClick={toggleTheme}
           className="fixed top-4 right-4 p-2 rounded-full bg-gray-200 dark:bg-gray-800"
